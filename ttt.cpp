c0fa3a4348f3affacd3bce6fc3b097fec6500a51
Ethan Tran <etra@730-ln11701-r.jeffco.schools>
2023-10-04 09:07:02 -0600

    added comments

--- ttt.cpp
+++ ttt.cpp
@@ -1,7 +1,16 @@
+/*
+Jack Flowers and Ethan Tran
+10/9/23
+Global Functions
+game()
+Description
+Just Tictactoe.
+*/
+
 #include <iostream>
 using namespace std;
 
-void game(int list[]){
+void game(int list[]){ //This is the game board
     printf("    a       b       c\n");
     printf("\t|\t|\n");
     printf("1   -   |   -   |   -\n");
@@ -15,11 +24,16 @@ void game(int list[]){
     printf("3   -   |   -   |   -\n");
     printf("\t|\t|\n");
 }
+
+//Game.
 int main(){
     game();
+    //declares array. Number is for amount of squares in tictactoe
     int memory[9];
     int turn = 1;
     string input;
+    //Used for deciding turns
+    //% 2; if even then player 2 if not player 1
     for(int i = 0; i < 9; i++){
         if(turn % 2 == 0){
             cout << "Player 2 ";
