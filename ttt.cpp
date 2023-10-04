935438d47980afec3310d37923f655ad7ccd3f64
John Flowers <jflo@730-ln11701-r.jeffco.schools>
2023-10-04 09:29:38 -0600

    yes

--- ttt.cpp
+++ ttt.cpp
@@ -16,15 +16,15 @@ using namespace std;
 void game(char list[]){ //This is the game board
     printf("    a       b       c\n");
     printf("\t|\t|\n");
-    printf("1   -   |   -   |   -\n");
+    printf("1   %c   |   %c   |   %c\n", list[0], list[3], list[6]);
     printf("\t|\t|\n");
     printf("-------------------------\n");
     printf("\t|\t|\n");
-    printf("2   -   |   -   |   -\n");
+    printf("2   %c   |   %c   |   %c\n", list[1], list[4], list[7]);
     printf("\t|\t|\n");
     printf("-------------------------\n");
     printf("\t|\t|\n");
-    printf("3   -   |   -   |   -\n");
+    printf("3   %c   |   %c   |   %c\n", list[2] list[5], list[8]);
     printf("\t|\t|\n");
 }
 
@@ -41,6 +41,7 @@ int main(){
     int row;
     //Used for deciding turns
     //% 2; if even then player 2 if not player 1
+    game(memory[]);
     for(int i = 0; i < 9; i++){
         if(turn % 2 == 0){
         //allows user to add columns and rows
