570cf99d4848044a35dcd6a0ea051c975367e1bc
John Flowers <jflo@730-ln11701-r.jeffco.schools>
2023-10-05 09:42:27 -0600

    nice

--- ttt.cpp
+++ ttt.cpp
@@ -24,7 +24,7 @@ void game(char list[]){ //This is the game board
     printf("\t|\t|\n");
     printf("-------------------------\n");
     printf("\t|\t|\n");
-    printf("3   %c   |   %c   |   %c\n", list[2] list[5], list[8]);
+    printf("3   %c   |   %c   |   %c\n", list[2], list[5], list[8]);
     printf("\t|\t|\n");
 }
 
@@ -33,7 +33,6 @@ void new_list(char column, int row){
 
 //Game.
 int main(){
-    game();
     //declares array. Number is for amount of squares in tictactoe
     char memory[] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
     int turn = 1;
@@ -41,23 +40,43 @@ int main(){
     int row;
     //Used for deciding turns
     //% 2; if even then player 2 if not player 1
-    game(memory[]);
+    game(memory);
     for(int i = 0; i < 9; i++){
         if(turn % 2 == 0){
         //allows user to add columns and rows
-            printf("Player 2\n\tEnter row: ");
+            printf("Player 2\nEnter row: ");
             cin >> row;
-            printf("\tEnter column: ");
+            printf("Enter column: ");
             cin >> column;
             turn += 1;
+            if(column == 'a'){
+                memory[(0 + row) - 1] = 'X';
+            }
+            else if(column == 'b'){
+                memory[(3 + row) - 1] = 'X';
+            }
+            else{
+                memory[(6 + row) - 1] = 'X';
+            }
+            game(memory);
         }
         else{
         //allows user to add columns and rows
-            printf("Player 1\n\tEnter row: ");
+            printf("Player 1\nEnter row: ");
             cin >> row;
-            printf("\tEnter column: ");
+            printf("Enter column: ");
             cin >> column;
             turn += 1;
+            if(column == 'a'){
+                memory[(0 + row) - 1] = 'O';
+            }
+            else if(column == 'b'){
+                memory[(3 + row) - 1] = 'O';
+            }
+            else{
+                memory[(6 + row) - 1] = 'O';
+            }
+            game(memory);
         }
     }
     return 0;
